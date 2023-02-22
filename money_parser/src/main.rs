3be3bfb8ae07502095706969c214c663ca3c1d38
Fabian Vilers <fabian.vilers@dev-one.com>
2023-02-22 10:25:41 +0100

    Implement the naive parse_money function

--- money_parser/src/main.rs
+++ money_parser/src/main.rs
@@ -1,23 +1,103 @@
-// The `use` keyword imports items for other crates or modules.
 use std::{env, process};
 
+// This is a magic number value indicating a parsing error. Don't do that at home, it's a temporary way of returning
+// meaningful information from the `parse_money()` function.
+const INVALID_INPUT: f32 = -1.0;
+
 fn main() {
-    // Let's collect the arguments that this program was started with and store them into vector. A vector is a
-    // contiguous growable array. `args` should contains two `String` elements: "target\\debug\\money_parser.exe" and
-    // "123.45 €"
     let args: Vec<String> = env::args().collect();
 
-    // Let's fail early in case the user forgot to pass a string containing a money representation
     if args.len() < 2 {
-        // `eprintln!()` is the same macro than `println!()` except that it writes to stderr
         eprintln!("Usage: money_parser.exe <value_to_parse>");
-
-        // Exit the process and return an error code to the Operating System
         process::exit(1);
     }
 
-    // `println!()` and `eprintln!()` macros are able to format the string and do variable interpolation. Each
-    // placeholder is substituted with the variable taken from the list in the same order.
-    // Note that we specify a special formatting parameter to display debug information about the variable.
-    println!("Args: {:?}", args);
+    // Call the `parse_money()` function with the second arguments (the first one is the executable path).
+    let (amount, currency) = parse_money(&args[1]);
+
+    // Display a message to the user if couldn't parse its input.
+    if amount == INVALID_INPUT {
+        eprintln!("Invalid input");
+        process::exit(1);
+    }
+
+    // If we made it so far, everything is ok, let's print the amount and currency..
+    println!("Amount: {}, currency: {}", amount, currency);
+}
+
+// The `parse_money()` function has one parameter which is a reference to a string (more on that later) and returns a
+// tuple of a signed 32-bit float and a reference to a string.
+fn parse_money(input: &str) -> (f32, &str) {
+    // Split the input string on the white space character and collect the segments into a vector.
+    let segments: Vec<&str> = input.split(' ').collect();
+
+    // Again, let's fail early if the vector length is not exactly of two segments.
+    if segments.len() != 2 {
+        return (INVALID_INPUT, "");
+    }
+
+    // The compiler cannot infer to which type the value should be parsed so we annotate the amount variable with `f32`.
+    // Don't pay attention to the `unwrap()` function yet.
+    let amount: f32 = segments[0].parse().unwrap();
+
+    // Look how convenient it is to omit the semi-colon and have the compiler use the expression as the return value!
+    (amount, segments[1])
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_money_should_return_invalid_input_const_for_empty_string() {
+        let (amount, _) = parse_money("");
+
+        assert_eq!(amount, INVALID_INPUT);
+    }
+
+    #[test]
+    fn parse_money_should_return_invalid_input_const_for_string_without_currency() {
+        let (amount, _) = parse_money("123");
+
+        assert_eq!(amount, INVALID_INPUT);
+    }
+
+    #[test]
+    fn parse_money_should_return_invalid_input_const_for_string_without_amount() {
+        let (amount, _) = parse_money("eur");
+
+        assert_eq!(amount, INVALID_INPUT);
+    }
+
+    #[test]
+    fn parse_money_should_return_integer_amount_and_currency() {
+        let (amount, currency) = parse_money("123 €");
+
+        assert_eq!(amount, 123.0);
+        assert_eq!(currency, "€");
+    }
+
+    #[test]
+    fn parse_money_should_return_float_amount_and_currency() {
+        let (amount, currency) = parse_money("123.45 €");
+
+        assert_eq!(amount, 123.45);
+        assert_eq!(currency, "€");
+    }
+
+    #[test]
+    fn parse_money_should_return_negative_integer_amount_and_currency() {
+        let (amount, currency) = parse_money("-123 €");
+
+        assert_eq!(amount, -123.0);
+        assert_eq!(currency, "€");
+    }
+
+    #[test]
+    fn parse_money_should_return_negative_float_amount_and_currency() {
+        let (amount, currency) = parse_money("-123.45 €");
+
+        assert_eq!(amount, -123.45);
+        assert_eq!(currency, "€");
+    }
 }
