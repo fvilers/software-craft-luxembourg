4028dbfe15d4087f7b0669f5385a95161f2a4a85
Fabian Vilers <fabian.vilers@dev-one.com>
2023-02-22 11:11:22 +0100

    Improve code using the FromStr trait

--- money_parser/src/main.rs
+++ money_parser/src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, process};
+use std::{env, process, str::FromStr};
 
 #[derive(Debug)]
 enum ParseMoneyError {
@@ -6,17 +6,37 @@ enum ParseMoneyError {
     ParseFailed,
 }
 
-// A Money struct type, with a lifetime annotation (read bellow).
-struct Money<'a> {
+struct Money {
     amount: f32,
-    currency: &'a str,
+    currency: String,
 }
 
-// The Money struct is implemented for a specific lifetime. This means that the struct must live long enough (as long as
-// the currency parameter will live).
-impl<'a> Money<'a> {
-    fn new(amount: f32, currency: &'a str) -> Self {
-        Self { amount, currency }
+impl Money {
+    fn new(amount: f32, currency: &str) -> Self {
+        Self {
+            amount,
+            currency: currency.to_string(),
+        }
+    }
+}
+
+// Implements the `FromStr` trait for our `Money` struct so that the compiler will be able to resolve the `parse()`
+// function on any `str` value.
+impl FromStr for Money {
+    // Declare a local type representing the error that will be returned.
+    type Err = ParseMoneyError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let segments: Vec<&str> = s.split_whitespace().collect();
+
+        match segments[..] {
+            [amount, currency] => amount
+                .parse::<f32>()
+                .map(|amount| Money::new(amount, currency))
+                .map_err(|_| ParseMoneyError::ParseFailed),
+
+            _ => Err(ParseMoneyError::InvalidInput),
+        }
     }
 }
 
@@ -26,7 +46,7 @@ fn main() {
         return;
     };
 
-    match parse_money(&user_input) {
+    match user_input.parse::<Money>() {
         Ok(money) => println!("Amount: {}, currency: {}", money.amount, money.currency),
         Err(error) => {
             eprintln!("Parsing error: {:?}", error);
@@ -35,71 +55,58 @@ fn main() {
     }
 }
 
-fn parse_money(input: &str) -> Result<Money, ParseMoneyError> {
-    let segments: Vec<&str> = input.split_whitespace().collect();
-
-    match segments[..] {
-        [amount, currency] => amount
-            .parse::<f32>()
-            .map(|amount| Money::new(amount, currency))
-            .map_err(|_| ParseMoneyError::ParseFailed),
-
-        _ => Err(ParseMoneyError::InvalidInput),
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn parse_money_should_return_invalid_input_const_for_empty_string() {
-        let result = parse_money("");
+    fn money_from_str_should_return_invalid_input_const_for_empty_string() {
+        let result = Money::from_str("");
 
         assert!(result.is_err());
     }
 
     #[test]
-    fn parse_money_should_return_invalid_input_const_for_string_without_currency() {
-        let result = parse_money("123");
+    fn money_from_str_should_return_invalid_input_const_for_string_without_currency() {
+        let result = Money::from_str("123");
 
         assert!(result.is_err());
     }
 
     #[test]
-    fn parse_money_should_return_invalid_input_const_for_string_without_amount() {
-        let result = parse_money("eur");
+    fn money_from_str_should_return_invalid_input_const_for_string_without_amount() {
+        let result = Money::from_str("eur");
 
         assert!(result.is_err());
     }
 
     #[test]
-    fn parse_money_should_return_integer_amount_and_currency() {
-        let money = parse_money("123 €").unwrap();
+    fn money_from_str_should_return_integer_amount_and_currency() {
+        let money = Money::from_str("123 €").unwrap();
 
         assert_eq!(money.amount, 123.0);
         assert_eq!(money.currency, "€");
     }
 
     #[test]
-    fn parse_money_should_return_float_amount_and_currency() {
-        let money = parse_money("123.45 €").unwrap();
+    fn money_from_str_should_return_float_amount_and_currency() {
+        let money = Money::from_str("123.45 €").unwrap();
 
         assert_eq!(money.amount, 123.45);
         assert_eq!(money.currency, "€");
     }
 
     #[test]
-    fn parse_money_should_return_negative_integer_amount_and_currency() {
-        let money = parse_money("-123 €").unwrap();
+    fn money_from_str_should_return_negative_integer_amount_and_currency() {
+        let money = Money::from_str("-123 €").unwrap();
 
         assert_eq!(money.amount, -123.0);
         assert_eq!(money.currency, "€");
     }
 
     #[test]
-    fn parse_money_should_return_negative_float_amount_and_currency() {
-        let money = parse_money("-123.45 €").unwrap();
+    fn money_from_str_should_return_negative_float_amount_and_currency() {
+        let money = Money::from_str("-123.45 €").unwrap();
 
         assert_eq!(money.amount, -123.45);
         assert_eq!(money.currency, "€");
