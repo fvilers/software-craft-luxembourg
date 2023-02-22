76cf5a6a86cc6f40b3ebb6eec7910b5650911109
Fabian Vilers <fabian.vilers@dev-one.com>
2023-02-22 09:31:15 +0100

    Add the money_parser package

---
+++ money_parser/src/main.rs
@@ -0,0 +1,6 @@
+// The main function is always the first code that runs.
+fn main() {
+    // `println!()` is a macro that writes a string to standard output. The macro is useful as it replaces code that
+    // locks the stdout handle and then writes to it.
+    println!("Hello, world!");
+}
